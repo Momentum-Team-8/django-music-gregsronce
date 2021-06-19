b242e0694672160157b00565d156cd669ef5cbd2
Greg Sronce <greg.sronce@gmail.com>
2021-06-19 16:06:47 -0400

    Added some styling and bulma.

--- django_music/urls.py
+++ django_music/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from albums import views as albums_views
-from django.conf.urls.static import static
 
 import albums
 
@@ -42,6 +41,3 @@ if settings.DEBUG:
         # For django versions before 2.0:
         # url(r'^__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
-
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
